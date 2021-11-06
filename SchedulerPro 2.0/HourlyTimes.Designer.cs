abe402864deb91dbb755ca36b3b550808c354c18
Sebastian Trill <strill@ksu.edu>
2021-11-06 17:24:31 -0500

    Added times for the emplyees. Also added in number of shifts available. Changed text boxes to rich text boxes.

--- SchedulerPro 2.0/HourlyTimes.Designer.cs
+++ SchedulerPro 2.0/HourlyTimes.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SchedulerPro_2._0
 {
-    partial class uxUserInterface
+    partial class HourlyTimes
     {
         /// <summary>
         /// Required designer variable.
@@ -29,31 +29,69 @@ namespace SchedulerPro_2._0
         /// </summary>
         private void InitializeComponent()
         {
-            this.uxOpenFile = new System.Windows.Forms.Button();
+            this.uxMondayText = new System.Windows.Forms.TextBox();
+            this.uxTuesdayText = new System.Windows.Forms.TextBox();
+            this.uxWednesdayText = new System.Windows.Forms.TextBox();
+            this.uxThursdayText = new System.Windows.Forms.TextBox();
+            this.uxFridayText = new System.Windows.Forms.TextBox();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
-            this.uxMondayText = new System.Windows.Forms.RichTextBox();
-            this.uxWednesdayText = new System.Windows.Forms.RichTextBox();
-            this.uxThursdayText = new System.Windows.Forms.RichTextBox();
-            this.uxFridayText = new System.Windows.Forms.RichTextBox();
-            this.uxTuesdayText = new System.Windows.Forms.RichTextBox();
             this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // uxOpenFile
+            // uxMondayText
             // 
-            this.uxOpenFile.Location = new System.Drawing.Point(161, 1340);
-            this.uxOpenFile.Name = "uxOpenFile";
-            this.uxOpenFile.Size = new System.Drawing.Size(325, 67);
-            this.uxOpenFile.TabIndex = 0;
-            this.uxOpenFile.Text = "Open File";
-            this.uxOpenFile.UseVisualStyleBackColor = true;
-            this.uxOpenFile.Click += new System.EventHandler(this.uxOpenFile_Click);
+            this.uxMondayText.Location = new System.Drawing.Point(12, 103);
+            this.uxMondayText.Multiline = true;
+            this.uxMondayText.Name = "uxMondayText";
+            this.uxMondayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxMondayText.Size = new System.Drawing.Size(555, 1131);
+            this.uxMondayText.TabIndex = 12;
+            this.uxMondayText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxTuesdayText
+            // 
+            this.uxTuesdayText.Location = new System.Drawing.Point(573, 103);
+            this.uxTuesdayText.Multiline = true;
+            this.uxTuesdayText.Name = "uxTuesdayText";
+            this.uxTuesdayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxTuesdayText.Size = new System.Drawing.Size(555, 1131);
+            this.uxTuesdayText.TabIndex = 13;
+            this.uxTuesdayText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxWednesdayText
+            // 
+            this.uxWednesdayText.Location = new System.Drawing.Point(1134, 103);
+            this.uxWednesdayText.Multiline = true;
+            this.uxWednesdayText.Name = "uxWednesdayText";
+            this.uxWednesdayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxWednesdayText.Size = new System.Drawing.Size(555, 1131);
+            this.uxWednesdayText.TabIndex = 14;
+            this.uxWednesdayText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxThursdayText
+            // 
+            this.uxThursdayText.Location = new System.Drawing.Point(1695, 103);
+            this.uxThursdayText.Multiline = true;
+            this.uxThursdayText.Name = "uxThursdayText";
+            this.uxThursdayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxThursdayText.Size = new System.Drawing.Size(555, 1131);
+            this.uxThursdayText.TabIndex = 15;
+            this.uxThursdayText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxFridayText
+            // 
+            this.uxFridayText.Location = new System.Drawing.Point(2256, 103);
+            this.uxFridayText.Multiline = true;
+            this.uxFridayText.Name = "uxFridayText";
+            this.uxFridayText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.uxFridayText.Size = new System.Drawing.Size(555, 1131);
+            this.uxFridayText.TabIndex = 16;
+            this.uxFridayText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // flowLayoutPanel2
             // 
@@ -62,17 +100,16 @@ namespace SchedulerPro_2._0
             this.flowLayoutPanel2.Controls.Add(this.label3);
             this.flowLayoutPanel2.Controls.Add(this.label4);
             this.flowLayoutPanel2.Controls.Add(this.label5);
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 16);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(2810, 70);
-            this.flowLayoutPanel2.TabIndex = 1;
+            this.flowLayoutPanel2.TabIndex = 11;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Indigo;
+            this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(185, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(185, 0, 3, 0);
             this.label1.Name = "label1";
@@ -83,9 +120,8 @@ namespace SchedulerPro_2._0
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Indigo;
+            this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(727, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(325, 0, 3, 0);
             this.label2.Name = "label2";
@@ -96,9 +132,8 @@ namespace SchedulerPro_2._0
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Indigo;
+            this.label3.BackColor = System.Drawing.Color.White;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(1244, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(285, 0, 3, 0);
             this.label3.Name = "label3";
@@ -109,9 +144,8 @@ namespace SchedulerPro_2._0
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Indigo;
+            this.label4.BackColor = System.Drawing.Color.White;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
             this.label4.Location = new System.Drawing.Point(1827, 0);
             this.label4.Margin = new System.Windows.Forms.Padding(275, 0, 3, 0);
             this.label4.Name = "label4";
@@ -122,9 +156,8 @@ namespace SchedulerPro_2._0
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Indigo;
+            this.label5.BackColor = System.Drawing.Color.White;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
             this.label5.Location = new System.Drawing.Point(2402, 0);
             this.label5.Margin = new System.Windows.Forms.Padding(325, 0, 3, 0);
             this.label5.Name = "label5";
@@ -132,86 +165,39 @@ namespace SchedulerPro_2._0
             this.label5.TabIndex = 4;
             this.label5.Text = "Friday";
             // 
-            // uxOpenFileDialog
-            // 
-            this.uxOpenFileDialog.FileName = "openFileDialog1";
-            // 
-            // uxMondayText
-            // 
-            this.uxMondayText.Location = new System.Drawing.Point(12, 99);
-            this.uxMondayText.Name = "uxMondayText";
-            this.uxMondayText.Size = new System.Drawing.Size(555, 1131);
-            this.uxMondayText.TabIndex = 11;
-            this.uxMondayText.Text = "";
-            // 
-            // uxWednesdayText
-            // 
-            this.uxWednesdayText.Location = new System.Drawing.Point(1134, 99);
-            this.uxWednesdayText.Name = "uxWednesdayText";
-            this.uxWednesdayText.Size = new System.Drawing.Size(555, 1131);
-            this.uxWednesdayText.TabIndex = 12;
-            this.uxWednesdayText.Text = "";
-            // 
-            // uxThursdayText
-            // 
-            this.uxThursdayText.Location = new System.Drawing.Point(1695, 99);
-            this.uxThursdayText.Name = "uxThursdayText";
-            this.uxThursdayText.Size = new System.Drawing.Size(555, 1131);
-            this.uxThursdayText.TabIndex = 13;
-            this.uxThursdayText.Text = "";
-            // 
-            // uxFridayText
-            // 
-            this.uxFridayText.Location = new System.Drawing.Point(2256, 99);
-            this.uxFridayText.Name = "uxFridayText";
-            this.uxFridayText.Size = new System.Drawing.Size(555, 1131);
-            this.uxFridayText.TabIndex = 14;
-            this.uxFridayText.Text = "";
-            // 
-            // uxTuesdayText
-            // 
-            this.uxTuesdayText.Location = new System.Drawing.Point(573, 99);
-            this.uxTuesdayText.Name = "uxTuesdayText";
-            this.uxTuesdayText.Size = new System.Drawing.Size(555, 1131);
-            this.uxTuesdayText.TabIndex = 15;
-            this.uxTuesdayText.Text = "";
-            // 
-            // uxUserInterface
+            // HourlyTimes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Indigo;
-            this.ClientSize = new System.Drawing.Size(2833, 1537);
-            this.Controls.Add(this.uxTuesdayText);
-            this.Controls.Add(this.uxFridayText);
-            this.Controls.Add(this.uxThursdayText);
-            this.Controls.Add(this.uxWednesdayText);
+            this.ClientSize = new System.Drawing.Size(2824, 1250);
             this.Controls.Add(this.uxMondayText);
+            this.Controls.Add(this.uxTuesdayText);
+            this.Controls.Add(this.uxWednesdayText);
+            this.Controls.Add(this.uxThursdayText);
+            this.Controls.Add(this.uxFridayText);
             this.Controls.Add(this.flowLayoutPanel2);
-            this.Controls.Add(this.uxOpenFile);
-            this.Name = "uxUserInterface";
-            this.Text = "SchedulerPro";
+            this.Name = "HourlyTimes";
+            this.Text = "Form1";
             this.flowLayoutPanel2.ResumeLayout(false);
             this.flowLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button uxOpenFile;
+        public System.Windows.Forms.TextBox uxMondayText;
+        private System.Windows.Forms.TextBox uxTuesdayText;
+        private System.Windows.Forms.TextBox uxWednesdayText;
+        private System.Windows.Forms.TextBox uxThursdayText;
+        private System.Windows.Forms.TextBox uxFridayText;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
-        private System.Windows.Forms.RichTextBox uxMondayText;
-        private System.Windows.Forms.RichTextBox uxWednesdayText;
-        private System.Windows.Forms.RichTextBox uxThursdayText;
-        private System.Windows.Forms.RichTextBox uxFridayText;
-        private System.Windows.Forms.RichTextBox uxTuesdayText;
     }
 }
-
--- SchedulerPro 2.0/Scheduler.cs
+++ SchedulerPro 2.0/Scheduler.cs
@@ -53,28 +53,85 @@ namespace SchedulerPro_2._0
                 data_values = sb.ReadLine().Split(',');
                 string name = data_values[0];
                 string[] days = data_values[1].Split(';');
+                string[] start_times = data_values[2].Split(';');
+                string[] end_times = data_values[3].Split(';');
 
                 foreach (string day in days)
                 {
                     if (day == "Monday" || day == "monday")
                     {
                         uxMondayText.Text = uxMondayText.Text + name + Environment.NewLine;
+
+                        for(int i = 0; i < start_times.Length; i++)
+                        {
+                            uxMondayText.Text = uxMondayText.Text + start_times[i] + " - " + end_times[i] + Environment.NewLine;
+                        }
+
+                        uxMondayText.Text = uxMondayText.Text + "(Available for " + start_times.Length + " shifts)" + Environment.NewLine;
+                        uxMondayText.Text = uxMondayText.Text + Environment.NewLine;
+
+                        uxMondayText.SelectAll();
+                        uxMondayText.SelectionAlignment = HorizontalAlignment.Center;
                     }
                     else if (day == "Tuesday" || day == "tuesday")
                     {
                         uxTuesdayText.Text = uxTuesdayText.Text + name + Environment.NewLine;
+
+                        for (int i = 0; i < start_times.Length; i++)
+                        {
+                            uxTuesdayText.Text = uxTuesdayText.Text + start_times[i] + " - " + end_times[i] + Environment.NewLine;
+                        }
+
+                        uxTuesdayText.Text = uxTuesdayText.Text + "(Available for " + start_times.Length + " shifts)" + Environment.NewLine;
+                        uxTuesdayText.Text = uxTuesdayText.Text + Environment.NewLine;
+
+                        uxTuesdayText.SelectAll();
+                        uxTuesdayText.SelectionAlignment = HorizontalAlignment.Center;
                     }
                     else if (day == "Wednesday" || day == "wednesday")
                     {
                         uxWednesdayText.Text = uxWednesdayText.Text + name + Environment.NewLine;
+
+                        for (int i = 0; i < start_times.Length; i++)
+                        {
+                            uxWednesdayText.Text = uxWednesdayText.Text + start_times[i] + " - " + end_times[i] + Environment.NewLine;
+                        }
+
+                        uxWednesdayText.Text = uxWednesdayText.Text + "(Available for " + start_times.Length + " shifts)" + Environment.NewLine;
+                        uxWednesdayText.Text = uxWednesdayText.Text + Environment.NewLine;
+
+                        uxWednesdayText.SelectAll();
+                        uxWednesdayText.SelectionAlignment = HorizontalAlignment.Center;
                     }
                     else if (day == "Thursday" || day == "thursday")
                     {
                         uxThursdayText.Text = uxThursdayText.Text + name + Environment.NewLine;
+
+                        for (int i = 0; i < start_times.Length; i++)
+                        {
+                            uxThursdayText.Text = uxThursdayText.Text + start_times[i] + " - " + end_times[i] + Environment.NewLine;
+                        }
+
+                        uxThursdayText.Text = uxThursdayText.Text + "(Available for " + start_times.Length + " shifts)" + Environment.NewLine;
+                        uxThursdayText.Text = uxThursdayText.Text + Environment.NewLine;
+
+                        uxThursdayText.SelectAll();
+                        uxThursdayText.SelectionAlignment = HorizontalAlignment.Center;
                     }
                     else if (day == "Friday" || day == "friday")
                     {
                         uxFridayText.Text = uxFridayText.Text + name + Environment.NewLine;
+
+                        for (int i = 0; i < start_times.Length; i++)
+                        {
+                            uxFridayText.Text = uxFridayText.Text + start_times[i] + " - " + end_times[i] + Environment.NewLine;
+                        }
+
+                        uxFridayText.Text = uxFridayText.Text + "(Available for " + start_times.Length + " shifts)" + Environment.NewLine;
+                        uxFridayText.Text = uxFridayText.Text + Environment.NewLine;
+
+                        uxFridayText.SelectAll();
+                        uxFridayText.SelectionAlignment = HorizontalAlignment.Center;
                     }
                     else
                     {
